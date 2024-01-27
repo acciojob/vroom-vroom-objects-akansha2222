723a144e381e026a22ecdd16fb9733345354ad9b
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2024-01-27 17:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 function Car(make, model) {
 	function getMakeModel()
 		{
-			return `$make , $model`
+		this.make = make
+		this.model = model
+		}
+		Car.prototype.getMakeModel = function()
+		{
+			return this.make + " " + this.model
 		}
 }
 
 function SportsCar(make, model, topSpeed) {
-	getTopSpeed()
-	{
-		return topSpeed
-	}
+		this.make = make
+		this.model = model
+	this.topSpeed = topSpeed
+	
 }
+SportsCar.prototype = Object.create(Car.prototype)
+
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function name(params) {
+	return this.TopSpeed 
+}
